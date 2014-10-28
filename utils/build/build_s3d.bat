7bd1fd4b874c454618d79db62c515a0c83f0f58d
spread3d <laurent@spread3d.com>
2014-10-28 13:30:21 +0100

    update naming convention

--- utils/build/build_s3d.bat
+++ utils/build/build_s3d.bat
@@ -1,4 +1,2 @@
-python build.py --include common --include extras --output ../../build/spread3d.full.three.js
-python build.py --include common --include extras --minify --output ../../build/spread3d.full.three.min.js
-python build.py --include s3dcore --output ../../build/spread3d.core.three.js
-python build.py --include s3dcore --minify --output ../../build/spread3d.core.three.min.js
+python build.py --include s3dcore --output ../../build/spread3d.three.js
+python build.py --include s3dcore --minify --output ../../build/spread3d.three.min.js
